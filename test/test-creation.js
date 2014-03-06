170125e6a5c4e9cc5fceb3127e3f849810a0cdd3
Hanusz Leszek <leszek.hanusz@gmail.com>
2014-03-06 17:17:18 +0100

    Add correct files and mock params for test-creation test

--- test/test-creation.js
+++ test/test-creation.js
@@ -21,11 +21,34 @@ describe('knockout-bootstrap generator', function () {
     var expected = [
       // add files you expect to exist here.
       '.jshintrc',
-      '.editorconfig'
+      '.editorconfig',
+      'Gruntfile.js',
+      'bower.json',
+      '.bowerrc',
+      'package.json',
+      '.yo-rc.json',
+      'README.md',
+      'test/bower.json',
+      'test/spec/test.js',
+      'app/robots.txt',
+      'app/favicon.ico',
+      'app/assets/scss/main.scss',
+      'app/assets/js/config.js',
+      'app/assets/js/main.js',
+      'app/jade/index.jade',
+      'app/jade/partials/_home.jade',
+      'app/jade/partials/_contact.jade',
+      'app/jade/partials/_about.jade',
+      'app/jade/partials/_bootstrap_layout.jade',
+      'app/jade/partials/_leftmenutabs.jade',
+      'app/jade/partials/_scripts.jade'
     ];
 
     helpers.mockPrompt(this.app, {
-      'someOption': true
+      'appname': 'some name',
+      'appdescription': 'some description',
+      'jade': true,
+      'sammy': true
     });
     this.app.options['skip-install'] = true;
     this.app.run({}, function () {
