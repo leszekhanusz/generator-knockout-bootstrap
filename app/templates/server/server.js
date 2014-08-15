1197130814b90ddecc76b4618aa401ba47e433c1
Hanusz Leszek <leszek.hanusz@gmail.com>
2014-08-15 10:09:43 +0200

    fix(app): Remove deprecated express function 'app.configure'

--- app/templates/server/server.js
+++ app/templates/server/server.js
@@ -3,36 +3,35 @@ var app = express()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server);
 
-app.configure(function() {
-  app.set('port', process.env.PORT || 9010);
-  console.log('port = ' + app.get('port'));
-});
+// all environments
+app.set('port', process.env.PORT || 9010);
+console.log('port = ' + app.get('port'));
 
 if('development' === app.get('env') || 'test' === app.get('env')) {
-  app.set('rootFolder', '<%= env.options.appPath %>');
+  app.set('rootFolder', 'app');
   console.log('Adding live-reload middleware');
   //TODO use livereload port defined in Gruntfile.js
   var liveReloadSnippet = require('connect-livereload')();
   app.use(liveReloadSnippet);
+
+  if('development' === app.get('env')) {
+    console.log('Running server in development mode');
+  }
+
+  if('test' === app.get('env')) {
+    console.log('Running server in test mode');
+    app.use(express.static(__dirname + '/../test'));
+  }
 }
 
-app.configure('development', function() {
-  console.log('Running server in development mode');
-});
-
-app.configure('test', function() {
-  console.log('Running server in test mode');
-  app.use(express.static(__dirname + '/../test'));
-});
-
-app.configure('production', function() {
+if('production' === app.get('env')) {
   console.log('Running server in production mode');
   app.set('rootFolder', 'dist');
   io.enable('browser client minification');   //send minified client
   io.enable('browser client etag');           // apply etag caching logic based on version number
   io.enable('browser client gzip');           // gzip the file
   io.set('log level', 1);                     // reduce logging
-});
+}
 
 app.use(express.static(__dirname + '/../' + app.get('rootFolder')));
 
